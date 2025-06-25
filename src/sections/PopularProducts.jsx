8e34e45212ae1e252e91d5409314480fbd5869ee
Ridwan <ridwanpr@outlook.com>
2025-06-25 14:24:13 +0700

    add popular product section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,25 @@
+import PopularProductCard from "../components/PopularProductCard";
+import { products } from "../constants";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="font-palanquin text-4xl font-bold">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="mt-2 font-montserrat text-slate-gray lg:max-w-lg">
+          Experience top-notch quality and style with our popular shoe range
+        </p>
+      </div>
+
+      <div className="mt-16 grid grid-cols-1 gap-14 sm:grid-cols-2 sm:gap-6 md:grid-cols-3 lg:grid-cols-4">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,42 @@
+import Button from "../components/Button.jsx";
+import { shoe8 } from "../assets/images/index.js";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="max-lg justify-between:flex-col max-container flex w-full items-center gap-10"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl font-bold capitalize lg:max-w-lg">
+          We Provide You
+          <br />
+          <span className="text-coral-red">Super</span>
+          <span className="text-coral-red"> Quality</span> Shoes
+        </h2>
+        <p className="info-text mt-4 lg:max-w-lg">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to improve your experience, providing an unmatched quality
+          and inovation, and a touch of elegance.
+        </p>
+        <p className="info-text mt-6 lg:max-w-lg">
+          Our dedication to detail and excellence to ensure your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+      <div className="flex flex-1 items-center justify-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
